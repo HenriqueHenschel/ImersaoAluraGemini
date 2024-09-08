7a3f260b40822db5f65dce1e14002a2410ecc6fc
HenriqueHenschel <henriquehenschel16@gmail.com>
2024-09-08 20:32:08 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+// Contém os dados da aplicação neste arquivo separado (por organização e boas práticas)
+
+let dados = [
+  {
+    titulo: "Rayssa Leal",
+    descricao: "Rayssa Leal, a Fadinha, é um fenômeno do skate mundial. Com apenas alguns anos de idade, já conquistou o mundo com seu talento e carisma, tornando-se um ícone da juventude e um exemplo de superação.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8"
+  },
+  {
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma das maiores promessas do judô brasileiro. Com técnica apurada e determinação, ela já conquistou diversos títulos importantes, sendo um dos destaques da nova geração do judô nacional.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judo judô judoca judoka"
+  },
+  {
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é, sem dúvida, uma das maiores atletas brasileiras de todos os tempos. A ginasta, com sua força, agilidade e determinação, conquistou o coração dos brasileiros e se tornou um símbolo de superação.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginastica ginástica ouro"
+  }
+];
